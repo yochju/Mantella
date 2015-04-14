b96d5e7e6e8c24e325c86e19552d3cf631a6a318
Wolff <wlfbck@gmail.com>
2015-04-15 00:57:45 +0200

    devel: added svm to cmakelists and mantella file. inserted constructor parameters to svm.

--- include/mantella_bits/helper/supportVectorMachine.hpp
+++ include/mantella_bits/helper/supportVectorMachine.hpp
@@ -2,7 +2,7 @@ namespace mant {
 
   class RankingSVM {
     public:
-      explicit RankingSVM() noexcept;
+      explicit RankingSVM(arma::Mat<double> X_training, unsigned int nx, unsigned int ntraining, unsigned int niter, double epsilon, arma::Mat<double> invertedSquaredC, arma::Col<double> p_Ci,double sigma_A, double sigma_Pow, arma::Col<double> xmean) noexcept;
       
     protected:
       
