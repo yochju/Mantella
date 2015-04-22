28bfda637a6f510f01578dacdfcb1f927a8fd756
Wolff <wlfbck@gmail.com>
2015-04-23 00:02:50 +0200

    devel: added lots of notices regarding RankSVMLearn variables and what actually happens in Encoding-method

--- include/mantella_bits/helper/supportVectorMachine.hpp
+++ include/mantella_bits/helper/supportVectorMachine.hpp
@@ -4,8 +4,14 @@ namespace mant {
     public:
       explicit RankingSVM(arma::Mat<double> X_training, unsigned int nx, unsigned int ntraining, unsigned int niter, double epsilon, arma::Mat<double> invertedSquaredC, arma::Col<double> p_Ci,double sigma_A, double sigma_Pow, arma::Col<double> xmean) noexcept;
       
-    protected:
       
+    protected:
+      DistPow2_Euclidean(arma::Col<double> x1, arma::Col<double> x2);
+      
+      double sigmaA = 1;
+      double sigmaPow = 1;
+      double epsilon = 1;
+      double
   };
 
   //
