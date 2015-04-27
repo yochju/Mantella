7e623047911751bae1ce18d3c43a00a66836a7ea
Wolff <wlfbck@gmail.com>
2015-04-28 00:23:36 +0200

    devel: finished notes on RankSVMLearn, ready for converting to c++

--- include/mantella_bits/helper/supportVectorMachine.hpp
+++ include/mantella_bits/helper/supportVectorMachine.hpp
@@ -1,9 +1,7 @@
 namespace mant {
 
   class RankingSVM {
-    public:
-      explicit RankingSVM(arma::Mat<double> X_training, unsigned int nx, unsigned int ntraining, unsigned int niter, double epsilon, arma::Mat<double> invertedSquaredC, arma::Col<double> p_Ci,double sigma_A, double sigma_Pow, arma::Col<double> xmean) noexcept;
-      
+    public:     
       
     protected:
       DistPow2_Euclidean(arma::Col<double> x1, arma::Col<double> x2);
@@ -11,7 +9,6 @@ namespace mant {
       double sigmaA = 1;
       double sigmaPow = 1;
       double epsilon = 1;
-      double
   };
 
   //
