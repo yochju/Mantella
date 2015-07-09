7afa74fe1bcf4438df7228f8d2fde9f681eee9bf
Wolff <wlfbck@gmail.com>
2015-07-09 16:54:35 +0200

    devel: added some variables

--- include/mantella_bits/optimisationAlgorithm/populationBasedOptimisationAlgorithm/HCMA.hpp
+++ include/mantella_bits/optimisationAlgorithm/populationBasedOptimisationAlgorithm/HCMA.hpp
@@ -8,9 +8,28 @@ namespace mant {
       
           std::string toString() const noexcept override;
       protected:
+        
+        unsigned int lambdaMult; //settings.lambdaMult
+        double sigma0 = 2.0; //later found in cur_state.insigma
 
       void optimiseImplementation() override;
   };
+  
+  //
+  //Implementation
+  //
+  
+  template <typename T>
+  HCMA<T>::HCMA(const std::shared_ptr<OptimisationProblem<T>> optimisationProblem,
+      const unsigned int populationSize) noexcept
+    : PopulationBasedOptimisationAlgorithm<T>(optimisationProblem, populationSize) {
+    if(this->numberOfDimensions_ < 10) {
+      lambdaMult = 1;
+    } else {
+      lambdaMult = floor(std::pow(10,1+std::log2(this->numberOfDimensions_*1.0/10)));
+    }
+    
+  }
   
   template <typename T>
   void HCMA<T>::optimiseImplementation() {
